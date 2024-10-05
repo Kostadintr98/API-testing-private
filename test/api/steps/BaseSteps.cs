aeb1aea6728947e5f3b7a140b4706a3db4f16461
Kostadin Trampov <kostadintr98@gmail.com>
2024-10-05 20:04:59 +0300

    fix

--- test/api/steps/BaseSteps.cs
+++ test/api/steps/BaseSteps.cs
@@ -9,7 +9,7 @@ namespace OnlineBookstore.test.api.steps;
 
 public class BaseSteps
 {
-    private static Random random = new Random();
+    private static Random random;
     private readonly RestClient _client;
     public readonly IConfiguration _config;
         
@@ -59,13 +59,11 @@ public class BaseSteps
 
     protected int GenerateRandomNumber(int low, int high)
     {
-        // Ensure low is less than high
         if (low >= high)
         {
             throw new ArgumentException("The 'low' parameter must be less than the 'high' parameter.");
         }
-
-        // Generate a random number in the specified range
+        
         return random.Next(low, high);
     }
 
@@ -81,7 +79,6 @@ public class BaseSteps
 
         for (int i = 0; i < length; i++)
         {
-            // Select a random character from the defined set
             char randomChar = chars[random.Next(chars.Length)];
             result.Append(randomChar);
         }
